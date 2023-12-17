ee70ece26ce5fd04d5e933e809f325ed1987a6ee
kazu0429 <k.shouhi0412@i.softbank.jp>
2023-12-17 11:56:59 +0900

    add chap19 editor

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -46,7 +46,9 @@ export const Item = ({
     ) => {
         event.stopPropagation();
         if(!id) return;
-        const promise = archive({ id });
+        const promise = archive({ id })
+        .then(() => router.push(`/documents`));
+
 
         toast.promise(promise, {
             loading: "Moving to trash...",
@@ -70,7 +72,7 @@ export const Item = ({
                 if (!expanded) {
                     onExpand?.();
                 }
-                // router.push(`/documents/${documentId}`);
+                router.push(`/documents/${documentId}`);
             },
         );
 
