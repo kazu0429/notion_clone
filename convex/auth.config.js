685eb0f9e83f7f9d6d1a9991ec8deb6fd49472cf
kazu0429 <k.shouhi0412@i.softbank.jp>
2023-12-17 16:06:41 +0900

    fix auth domain

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://tops-albacore-95.clerk.accounts.dev",
+      domain: "https://tops-albacore-95.clerk.accounts.dev/",
       applicationID: "convex",
     },
   ],
